5ca042ce89c0e76064f4be1f463827bc280f8588
marilin <puchetamarilin@gmail.com>
2024-04-19 01:10:34 -0300

    Cambios

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { ActividadesModule } from './actividades/actividades.module';
+import { AuditoriaModule } from './auditoria/auditoria.module';
+import { ClientesModule } from './clientes/clientes.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsuariosModule, ActividadesModule, AuditoriaModule, ClientesModule],
   controllers: [],
   providers: [],
 })
