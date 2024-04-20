9cb46f6fe1b5bd0c15e3db29931bee0e2fa71df5
marilin <puchetamarilin@gmail.com>
2024-04-20 13:33:32 -0300

    Cambios

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./controllers/autn.controller";
 import { AuthService } from "./services/auth.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Usuario } from "./entities/usuario.entity";
 
 
 @Module({
     controllers:[AuthController],
     providers:[AuthService],
-    imports:[TypeOrmModule.forFeature({Usuario})],
+    imports:[TypeOrmModule.forFeature([Usuario])],
     exports:[]
 })
 export class AuthModule{}
--- backend/src/auth/services/auth.service.ts
+++ backend/src/auth/services/auth.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { loginDto } from "../dtos/login.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Usuario } from "../entities/usuario.entity";
+import { Repository } from "typeorm";
 
 
 @Injectable()
 export class AuthService{
+    constructor(@InjectRepository(Usuario) private usuariosRepo:Repository<Usuario>){
 
-    login(loginDto:loginDto){
-        console.log('funciona service')
+    }
+
+    async login(loginDto:loginDto){
+        // console.log('funciona service');
+        const usuario:Usuario = await this.usuariosRepo.findOne({
+            where:{
+                nombreusuario:loginDto.nombreUsuario,
+            },
+        });
+        if(!Usuario){
+            throw new BadRequestException('Nombre de usuario no valido')
+        }
     }
 }
