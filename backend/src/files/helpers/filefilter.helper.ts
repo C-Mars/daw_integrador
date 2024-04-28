4c511a302b5aded4100a0091eaf65b3e1c38d046
marilin <puchetamarilin@gmail.com>
2024-04-27 21:11:40 -0300

    cambios

--- backend/src/files/helpers/filefilter.helper.ts
+++ backend/src/files/helpers/filefilter.helper.ts
@@ -1,15 +1,15 @@
 
 
-export const fileFilter=( req: Express.Request, file:Express.Multer.File, callback: Function)=>{
+export const fileFilter = ( req: Express.Request, file:Express.Multer.File, callback: Function)=>{
 
  if(!file) return callback(new Error('Archivo vacio.'), false);
 
  //Creo ctes para verificar si corresponden al formato de archivo de imagen
- const fileExptension = file.mimetype.split('/')[1];
- const validExtension = ['jpg','jpeg','png'];
+ const fileExptension = file.mimetype.split('.')[1];
+ const validExtension = ['.jpg','.jpeg','.png'];
 
  if(validExtension.includes(fileExptension)){
-return callback(null ,true ) //acepta el archivo
+    return callback(null ,true ) //acepta el archivo
  }
- callback(null, false);//no hacepta el archivo
+ callback(new Error('Formato de archivo no permitido.'), false);//no hacepta el archivo
 }
