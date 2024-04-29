418468e38d27c368f179cceac1253260638f409b
marilin <puchetamarilin@gmail.com>
2024-04-28 23:33:04 -0300

    Cambios
    Autorización Cliente

--- backend/src/clientes/clientes.controller.ts
+++ backend/src/clientes/clientes.controller.ts
@@ -5,7 +5,8 @@ import {
     Body,
     Patch,
     Param,
-    Delete
+    Delete,
+    UseGuards
 
 } from "@nestjs/common";
 
@@ -16,27 +17,44 @@ import { CreateClientesDto } from "./dto/create-clientes.dto";
 import { UpdateClientesDto } from "src/clientes/dto/update-clientes.dto";
 
 import { ClientesService } from "./clientes.service";
+import { ApiBearerAuth } from "@nestjs/swagger";
+import { RolesEnum } from "src/auth/enums/roles.enum";
+import { Roles } from "src/auth/decorators/roles.decorator";
+import { AuthGuard } from "src/auth/guards/auth.guard";
 
 @Controller("clientes")
 export class ClientesController {
 
     constructor(private clienteService: ClientesService) {
     }
+
     @Get()
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @UseGuards(AuthGuard)
     getAllClientes() {
 
         return this.clienteService.getAllCliente();
     }
 
     @Post()
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @UseGuards(AuthGuard)
     addNewCliente(@Body(ValidationPipe) data: CreateClientesDto) {
         return this.clienteService.addNewCliente(data);
     }
     @Patch(":id")
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @UseGuards(AuthGuard)
     UpdateCliente(@Param("id") id: string, @Body() UpdateClienteDto: UpdateClientesDto) {
         return this.clienteService.UpdateCliente(UpdateClienteDto, id);
     }
     @Delete("id")
+    @ApiBearerAuth()
+    @Roles([RolesEnum.ADMINISTRADOR])
+    @UseGuards(AuthGuard)
     deleteCliente(@Param("id") id: string) {
         return this.clienteService.deleteCliente(id);
     }
