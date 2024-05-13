7c3bbb4c1b19a6c016f06fe30345205700f643e0
marilin <puchetamarilin@gmail.com>
2024-05-13 15:00:01 -0300

    Cambios
    Frontend

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl:'./login.component.css',
+    selector:'app-login',
+    imports:[]
+})
+export class LoginComponent {
+
+}
