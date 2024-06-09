040656fe169d669ef9e45e6f1642581f9b6aa0b2
MoreyraE <ezemoreyravirtual@gmail.com>
2024-06-09 00:13:09 -0300

    ver todas las actividades api/actividades

--- backend/src/actividades/controllers/actividades.controller.ts
+++ backend/src/actividades/controllers/actividades.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Req, UseGuards, } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { ActividadesService } from '../services/actividades.service';
@@ -11,48 +11,48 @@ import { EditarActividadDto } from '../dtos/editar-actividades.dto';
 @ApiTags('actividades')
 @Controller('/actividades')
 export class ActividadesController {
-    constructor(private actividadService: ActividadesService) { }
+  constructor(private actividadService: ActividadesService) {}
 
-    @Post()
-    @ApiBearerAuth()
-    @Roles([RolesEnum.ADMINISTRADOR])
-    @UseGuards(AuthGuard)
-    async crearActividad(
-        @Req() request: Request,
-        @Body() crearActvidadDto: CrearActividadDto) {
-      
-        await this.actividadService.crearActividad(
-            crearActvidadDto,
-            request['usuario'],
-        )
-    }
+  @Post()
+  @ApiBearerAuth()
+  @Roles([RolesEnum.ADMINISTRADOR])
+  @UseGuards(AuthGuard)
+  async crearActividad(
+    @Req() request: Request,
+    @Body() crearActvidadDto: CrearActividadDto,
+  ) {
+    await this.actividadService.crearActividad(
+      crearActvidadDto,
+      request['usuario'],
+    );
+  }
 
+  @Get(':id')
+  @ApiBearerAuth()
+  @Roles([RolesEnum.ADMINISTRADOR])
+  @UseGuards(AuthGuard)
+  async getActividad(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Actividades> {
+    return this.actividadService.obtenerActividadPorId(id);
+  }
 
-    @Get(':id')
-    @ApiBearerAuth()
-    @Roles([RolesEnum.ADMINISTRADOR])
-    @UseGuards(AuthGuard)
-    async getActividad(
-        @Param('id', ParseIntPipe) id: number): Promise<Actividades> {
-        return this.actividadService.obtenerActividadPorId(id);
-    }
+  @Get()
+  @ApiBearerAuth()
+  @Roles([RolesEnum.ADMINISTRADOR])
+  @UseGuards(AuthGuard)
+  async getActividades(): Promise<Actividades[]> {
+    return this.actividadService.obtenerTodasLasActividades();
+  }
 
-    @Get()
-    @ApiBearerAuth()
-    @Roles([RolesEnum.ADMINISTRADOR])
-    @UseGuards(AuthGuard)
-    async getActividades() { }
-
-
-    @Patch(':id')
-    @ApiBearerAuth()
-    @Roles([RolesEnum.ADMINISTRADOR, RolesEnum.EJECUTOR])
-    @UseGuards(AuthGuard)
-    async editarActividad(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() editarActividadDto: EditarActividadDto
-    ) {
-        return await this.actividadService.editarActividad(id, editarActividadDto);
-    }
-
+  @Patch(':id')
+  @ApiBearerAuth()
+  @Roles([RolesEnum.ADMINISTRADOR, RolesEnum.EJECUTOR])
+  @UseGuards(AuthGuard)
+  async editarActividad(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() editarActividadDto: EditarActividadDto,
+  ) {
+    return await this.actividadService.editarActividad(id, editarActividadDto);
+  }
 }
--- backend/src/actividades/services/actividades.service.ts
+++ backend/src/actividades/services/actividades.service.ts
@@ -9,99 +9,114 @@ import { Usuario } from 'src/usuarios/entities/usuario.entity';
 import { RolesEnum } from 'src/auth/enums/roles.enum';
 import { EditarActividadDto } from '../dtos/editar-actividades.dto';
 
-
-
-
 @Injectable()
 export class ActividadesService {
-    constructor(
-        @InjectRepository(Actividades) private actividadesRepo: Repository<Actividades>,
-        private usuariosServices: UsuariosService,
+  constructor(
+    @InjectRepository(Actividades)
+    private actividadesRepo: Repository<Actividades>,
+    private usuariosServices: UsuariosService,
+  ) {}
 
-    ) { }
+  // Obtenemos una actividad por su ID
+  async obtenerActividadPorId(id: number): Promise<Actividades> {
+    const actividad = await this.actividadesRepo.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!actividad) {
+      throw new UnauthorizedException('La actividad no existe o fue eliminada');
+    }
+    return actividad;
+  }
 
-    //Obtenemos un array de actividades con un promise 
-    async obtenerActividadPorId(id: number): Promise<Actividades> {
-        const actividad = await this.actividadesRepo.findOne({
-            where: {
-                id
-            }
+  // Obtenemos todas las actividades
+  async obtenerTodasLasActividades(): Promise<Actividades[]> {
+    return await this.actividadesRepo.find();
+  }
+
+  // Crear una nueva actividad
+  async crearActividad(crearActividadDto: CrearActividadDto, usuario: Usuario) {
+    const nuevaActividad: Actividades = this.actividadesRepo.create();
+    nuevaActividad.idCliente = crearActividadDto.idCliente;
+    nuevaActividad.descripcion = crearActividadDto.descripcion;
+    nuevaActividad.estado = EstadoActividad.PENDIENTE;
+    nuevaActividad.fechaModificacion = new Date();
+    nuevaActividad.fechaInicio = new Date();
+    nuevaActividad.prioridad = crearActividadDto.prioridad;
+    nuevaActividad.usuarioActual = await this.usuariosServices.findOneById(
+      crearActividadDto.idUsuarioActual,
+    );
+    nuevaActividad.idUsuarioModificacion =
+      await this.usuariosServices.findOneById(
+        crearActividadDto.idUsuarioModificacion,
+      );
+    await this.actividadesRepo.save(nuevaActividad);
+  }
+
+  // Obtener actividades según el rol del usuario
+  async getActividades(usuario: Usuario): Promise<Actividades[]> {
+    const rol: RolesEnum = usuario.rol;
+
+    const consulta = this.actividadesRepo
+      .createQueryBuilder('actividad')
+      .innerJoin('actividad.usuarioActual', 'usuario');
+
+    if (rol === RolesEnum.EJECUTOR) {
+      consulta
+        .where('actividad.estado = :estado', {
+          estado: EstadoActividad.PENDIENTE,
+        })
+        .andWhere('usuario.id = :idUsuario', {
+          idUsuario: usuario.id,
         });
-        if (!actividad) {
-            throw new UnauthorizedException(
-                'La actividad no existe o fue eliminada'
-            );
-        }
-        return actividad;
     }
 
+    return await consulta.getMany();
+  }
 
-    async crearActividad(crearActividadDto: CrearActividadDto, usuario: Usuario) {
-        const nuevaActividad: Actividades = this.actividadesRepo.create();
-        nuevaActividad.idCliente= crearActividadDto.idCliente,
-        nuevaActividad.descripcion = crearActividadDto.descripcion;
-        nuevaActividad.estado = EstadoActividad.PENDIENTE;
-        nuevaActividad.fechaModificacion = new Date();
-        nuevaActividad.fechaInicio = new Date();
-        nuevaActividad.prioridad = crearActividadDto.prioridad;
-        nuevaActividad.usuarioActual = await this.usuariosServices.findOneById(
-            crearActividadDto.idUsuarioActual,
-        );
-        nuevaActividad.idUsuarioModificacion = await this.usuariosServices.findOneById(
-            crearActividadDto.idUsuarioModificacion,
-        );
-        await this.actividadesRepo.save(nuevaActividad);
+  // Editar una actividad
+  async editarActividad(id: number, editarActividadDto: EditarActividadDto) {
+    const actividad = await this.actividadesRepo.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!actividad) {
+      throw new UnauthorizedException('La actividad no existe o fue eliminada');
     }
 
-    async getActividades(usuario: Usuario): Promise<Actividades[]> {
-        const rol: RolesEnum = usuario.rol;
-
-        const consulta = this.actividadesRepo
-            .createQueryBuilder('actividad')
-            .innerJoin('actividad.usuarioActual', 'usuario');
-
-        if (rol === RolesEnum.EJECUTOR) {
-            consulta.where('actividad.estado = :estado', {
-                estado: EstadoActividad.PENDIENTE
-            }).andWhere('usuario.id = :idUsuario', {
-                idUsuario: usuario.id
-            });
-        }
-
-        return await consulta.getMany();
+    if (editarActividadDto.estado !== undefined) {
+      actividad.estado = editarActividadDto.estado;
     }
-    
-    
-    async editarActividad(id: number, editaraAtividadDto: EditarActividadDto) {
-        const actividad = await this.actividadesRepo.findOne({
-            where:{
-                id
-            }
-        });
-        if (!actividad) {
-            throw new UnauthorizedException('La actividad no existe o fue eliminada');
-        }
-        
-        actividad.estado = editaraAtividadDto.estado
-        actividad.descripcion = editaraAtividadDto.descripcion;
-        actividad.fechaInicio = editaraAtividadDto.fechaInicio;
-        actividad.fechaModificacion = editaraAtividadDto.fechaModificacion;
-        actividad.prioridad = editaraAtividadDto.prioridad
-        actividad.idCliente = editaraAtividadDto.idCliente
-       
-        await this.actividadesRepo.save(actividad);
-    
-        return actividad;
+    if (editarActividadDto.descripcion !== undefined) {
+      actividad.descripcion = editarActividadDto.descripcion;
+    }
+    if (editarActividadDto.fechaInicio !== undefined) {
+      actividad.fechaInicio = editarActividadDto.fechaInicio;
+    }
+    if (editarActividadDto.fechaModificacion !== undefined) {
+      actividad.fechaModificacion = editarActividadDto.fechaModificacion;
+    }
+    if (editarActividadDto.prioridad !== undefined) {
+      actividad.prioridad = editarActividadDto.prioridad;
+    }
+    if (editarActividadDto.idCliente !== undefined) {
+      actividad.idCliente = editarActividadDto.idCliente;
     }
 
+    await this.actividadesRepo.save(actividad);
 
-    async borrarActividad(id: number, usuario: Usuario) {
-        const rol: RolesEnum = usuario.rol;
-        
-        if (rol === RolesEnum.EJECUTOR || rol === RolesEnum.ADMINISTRADOR ) {
-        await this.actividadesRepo.delete(id);
-      }
+    return actividad;
+  }
+
+  // Borrar una actividad
+  async borrarActividad(id: number, usuario: Usuario) {
+    const rol: RolesEnum = usuario.rol;
+
+    if (rol === RolesEnum.EJECUTOR || rol === RolesEnum.ADMINISTRADOR) {
+      await this.actividadesRepo.delete(id);
+    }
+  }
 }
 
-
-}
