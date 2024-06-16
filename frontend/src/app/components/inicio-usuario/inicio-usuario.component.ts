b2cf494b5734bccbbc46898d2c91b2c3235a653f
marilin <puchetamarilin@gmail.com>
2024-06-16 00:38:41 -0300

    Cambios

--- frontend/src/app/components/inicio-usuario/inicio-usuario.component.ts
+++ frontend/src/app/components/inicio-usuario/inicio-usuario.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-inicio-usuario',
   standalone: true,
-  imports: [BaseComponent],
+  imports: [BaseComponent,ImageModule],
   templateUrl: './inicio-usuario.component.html',
   styleUrl: './inicio-usuario.component.scss'
 })
