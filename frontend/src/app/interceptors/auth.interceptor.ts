6be424e35b802243922235df461ed0a415b19771
marilin <puchetamarilin@gmail.com>
2024-06-17 21:03:33 -0300

    Cambios

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,12 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
-  const token:string|null = sessionStorage.getItem('token');
-  if (token) {
-    const cloned = req.clone({
-      headers: req.headers.set('Authorization', 'Bearer ' + token),
-    });
-    return next(cloned);
+  if (isPlatformBrowser(inject(PLATFORM_ID))) {
+    const token: string | null = sessionStorage.getItem('token');
+
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', 'Bearer ' + token),
+      });
+      return next(cloned);
+    }
   }
+
   return next(req);
 };
