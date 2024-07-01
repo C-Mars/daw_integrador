87f88bd7b8fcf142d90642000ec23f45f2b72af7
MoreyraE <ezemoreyravirtual@gmail.com>
2024-07-01 15:41:12 -0300

    Mostrar rol el usuario registrado

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+/*
 import { ChangeDetectorRef, Component, EventEmitter, Inject, Output, PLATFORM_ID, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth.service';
@@ -11,9 +12,9 @@ import { AjustesUsuarioComponent } from '../ajustes-usuario/ajustes-usuario.comp
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ButtonModule, 
-    ToolbarModule, 
-    NgClass, 
+  imports: [ButtonModule,
+    ToolbarModule,
+    NgClass,
     NgIf,
     MenuFooterComponent,
     TooltipModule,
@@ -43,7 +44,7 @@ export class HeaderComponent {
   onLoginClick() {
     this.loginClicked.emit();
   }
-   
+
   isLoggedView(): boolean {
     if (isPlatformBrowser(this.platformId)){
       const token = this._authService.isLogged();
@@ -55,20 +56,20 @@ export class HeaderComponent {
   // onAjustesUsuario() {
   //   this.ajustesUsuario.emit();
   // }
-  
+
   cerrarSesion() {
     this._authService.logout();
   }
 
   onMenuFooter(){
     this.sidebarVisible = !this.sidebarVisible;
-    this.cd.detectChanges(); 
+    this.cd.detectChanges();
   }
 
-  
+
   onAjustesUsuario(){
     this.dialogVisible = !this.dialogVisible;
-    this.cd.detectChanges(); 
+    this.cd.detectChanges();
   }
 
   onUsersClick() {
@@ -80,6 +81,105 @@ export class HeaderComponent {
   onHomeUsuClick() {
     this._router.navigateByUrl('/inicio-usuarios');
   }
+  isEjecutor(): boolean {
+    return this._authService.hasRole(RolesEnum.EJECUTOR);
+  }
+}
+
+*/
+// incoporacion del nombre y rol del usuario logeado
+import { ChangeDetectorRef, Component, EventEmitter, Inject, Output, PLATFORM_ID, inject } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../services/auth.service';
+import { ToolbarModule } from 'primeng/toolbar';
+import { NgClass, NgIf, isPlatformBrowser } from '@angular/common';
+import { Router } from '@angular/router';
+import { MenuFooterComponent } from '../menu-footer/menu-footer.component';
+import { RolesEnum } from '../../enums/roles.enum';
+import { TooltipModule } from 'primeng/tooltip';
+import { AjustesUsuarioComponent } from '../ajustes-usuario/ajustes-usuario.component';
+import { UsuarioDto } from '../../dtos/usuario.dto';
+import { AvatarModule } from 'primeng/avatar';
+import { BadgeModule } from 'primeng/badge';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    ToolbarModule,
+    NgClass,
+    NgIf,
+    MenuFooterComponent,
+    TooltipModule,
+    AjustesUsuarioComponent,
+    AvatarModule,
+    BadgeModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+
+export class HeaderComponent {
+  menuOption: string = '';
+  sidebarVisible: boolean = false;
+  dialogVisible: boolean = false;
+  usuario: UsuarioDto | null = null;
+
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
+  private readonly platformId = inject(PLATFORM_ID);
+
+  constructor(private cd: ChangeDetectorRef) {
+    if (this.isLoggedView()) {
+      this.usuario = this._authService.getUsuario();
+    }
+  }
+
+  onLoginSuccess() {
+    this.usuario = this._authService.getUsuario();
+  }
+
+  @Output() loginClicked = new EventEmitter<void>();
+  @Output() usersClicked = new EventEmitter<void>();
+
+  onLoginClick() {
+    this.loginClicked.emit();
+  }
+
+  isLoggedView(): boolean {
+    if (isPlatformBrowser(this.platformId)) {
+      return this._authService.isLogged();
+    }
+    return false;
+  }
+
+  cerrarSesion() {
+    this._authService.logout();
+  }
+
+  onMenuFooter() {
+    this.sidebarVisible = !this.sidebarVisible;
+    this.cd.detectChanges();
+  }
+
+  onAjustesUsuario() {
+    this.dialogVisible = !this.dialogVisible;
+    this.cd.detectChanges();
+  }
+
+  onUsersClick() {
+    this.usersClicked.emit();
+  }
+
+  onHomeClick() {
+    this._router.navigateByUrl('/inicio-admin');
+  }
+
+  onHomeUsuClick() {
+    this._router.navigateByUrl('/inicio-usuarios');
+  }
+
   isEjecutor(): boolean {
     return this._authService.hasRole(RolesEnum.EJECUTOR);
   }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+/*
 import { HttpClient } from '@angular/common/http';
 import { Injectable, PLATFORM_ID, inject } from '@angular/core';
 import { Router } from '@angular/router';
@@ -27,18 +28,18 @@ export class AuthService {
     });
   }
 
- 
+
   setSession(token: string) {
     sessionStorage.setItem('token', token);
   }
 
-  
+
 
   logout() {
     if (isPlatformBrowser(this.platformId)) {
     sessionStorage.removeItem('token');
     this._router.navigateByUrl('inicio');
-  } 
+  }
     return false
   }
 
@@ -50,23 +51,23 @@ export class AuthService {
     if (!token) {
       return false;
     }
-  
+
     return !new JwtHelperService().isTokenExpired(token);
-    
+
   }
 
   hasRole(rol: RolesEnum): boolean {
     if (isPlatformBrowser(this.platformId)){
       const token = sessionStorage.getItem('token');
-  
+
       if (!token) {
         return false;
       };
-  
+
       const userRole = new JwtHelperService().decodeToken(token).rol;
       return userRole === rol;
     }
-    
+
     return false;
   }
 
@@ -75,5 +76,103 @@ export class AuthService {
       return sessionStorage.getItem('token') ? true : false;
     }
     return false;
+  }
+}
+
+*/
+// incorporacion de los datos del usuario logeado
+import { HttpClient } from '@angular/common/http';
+import { Injectable, PLATFORM_ID, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Observable } from 'rxjs';
+import { RolesEnum } from '../enums/roles.enum';
+import { environment } from '../environments/environment';
+import { isPlatformBrowser } from '@angular/common';
+import { UsuarioDto } from '../dtos/usuario.dto';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly platformId = inject(PLATFORM_ID);
+
+  constructor(
+    private _client: HttpClient,
+    private _router: Router) {
+
+  }
+
+  login(nombreUsuario: string, clave: string): Observable<{ token: string }> {
+    return this._client.post<{ token: string }>(environment.apiUrl + '/auth/login', {
+      nombreUsuario,
+      clave,
+    });
+  }
+
+  setSession(token: string) {
+    sessionStorage.setItem('token', token);
+  }
+
+  logout() {
+    if (isPlatformBrowser(this.platformId)) {
+      sessionStorage.removeItem('token');
+      this._router.navigateByUrl('inicio');
+    }
+    return false;
+  }
+
+  isLoggedIn(): boolean {
+    if (!isPlatformBrowser(this.platformId)) {
+      return false;
+    }
+    const token = sessionStorage.getItem('token');
+    if (!token) {
+      return false;
+    }
+    return !new JwtHelperService().isTokenExpired(token);
+  }
+
+  hasRole(rol: RolesEnum): boolean {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = sessionStorage.getItem('token');
+
+      if (!token) {
+        return false;
+      }
+
+      const userRole = new JwtHelperService().decodeToken(token).rol;
+      return userRole === rol;
+    }
+    return false;
+  }
+
+  isLogged(): boolean {
+    if (isPlatformBrowser(this.platformId)) {
+      return sessionStorage.getItem('token') ? true : false;
+    }
+    return false;
+  }
+
+  getUsuario(): UsuarioDto | null {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = sessionStorage.getItem('token');
+      if (token) {
+        const decodedToken = new JwtHelperService().decodeToken(token);
+        return {
+          id: decodedToken.id,
+          nombres: decodedToken.nombres,
+          apellidos: decodedToken.apellidos,
+          email: decodedToken.email,
+          foto: decodedToken.foto,
+          fotoUrl: decodedToken.fotoUrl,
+          rol: decodedToken.rol,
+          nombreUsuario: decodedToken.nombreUsuario,
+          clave: null,
+          estado: decodedToken.estado,
+          //nombreCompleto:decodedToken.nombreCompleto
+        };
+      }
+    }
+    return null;
   }
 }
